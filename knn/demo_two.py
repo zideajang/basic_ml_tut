42b2267f137944f49246c8d7847115a640dd897e
zhangyv <zidea@163.com>
2019-11-06 17:25:08 +0800

    python 实现knn“

--- knn/demo_two.py
+++ knn/demo_two.py
@@ -11,6 +11,50 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+def train(X_train, y_train):
+    # do nothing
+    return
+def predict(X_train, y_train, x_test, k):
+    # create list for distances and targets
+    distances = []
+    targets = []
+
+    for i in range(len(X_train)):
+        # compute and store L2 distance
+        distances.append([np.sqrt(np.sum(np.square(x_test - X_train[i, :]))), i])
+
+    # sort the list
+    distances = sorted(distances)
+
+    # make a list of the k neighbors' targets
+    for i in range(k):
+        index = distances[i][1]
+        targets.append(y_train[index])
+
+    # return most common target
+    return Counter(targets).most_common(1)[0][0]
+def k_nearest_neighbor(X_train, y_train, X_test, k):
+    # train on the input data
+    train(X_train, y_train)
+
+    # loop over all observations
+    predictions = []
+    for i in range(len(X_test)):
+        predictions.append(predict(X_train, y_train, X_test[i, :], k))
+
+    return np.asarray(predictions)
+def knn_classifier():
+    pass
+def k_nearest_neighbor(X_train, y_train, X_test, predictions, k):
+    # check if k larger than n
+    assert k <= len(X_train), "[!] k can't be larger than number of samples."
+
+    # train on the input data
+    train(X_train, y_train)
+
+	# predict for each testing observation
+	for i in range(len(X_test)):
+        predictions.append(predict(X_train, y_train, X_test[i, :], k))
 
 def iris_classifier():
     print("iris classifier")
@@ -28,16 +72,16 @@ def iris_classifier():
     y = np.array(df['class'])
     # print(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    # instantiate learning model (k = 3)
+    # 实例化学习模型(k = 3)
     knn = KNeighborsClassifier(n_neighbors=3)
 
-    # fitting the model
+    # 训练模型
     knn.fit(X_train, y_train)
 
-    # predict the response
+    # 预测
     pred = knn.predict(X_test)
 
-    # evaluate accuracy
+    # 评估准确度
     print("accuracy: {}".format(accuracy_score(y_test, pred)))
 
 def car_classifier():
