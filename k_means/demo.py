d1325a3b3391b3691078672b663c5ae35632435e
zhangyv <zidea@163.com>
2019-11-12 17:02:45 +0800

    提交代码

--- k_means/demo.py
+++ k_means/demo.py
@@ -1,11 +1,12 @@
+# coding=utf-8
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
+# 创建数据集
 Data = {'x': [25,34,22,27,33,33,31,22,35,34,67,54,57,43,50,57,59,52,65,47,49,48,35,33,44,45,38,43,51,46],
         'y': [79,51,53,78,59,74,73,57,69,75,51,32,40,47,53,36,35,58,59,50,25,20,14,12,20,5,29,27,8,7]
        }
-  
+# 格式化数据
 df = DataFrame(Data,columns=['x','y'])
 # print (df)
 kmeans = KMeans(n_clusters=3).fit(df)
