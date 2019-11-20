f0fe7339d0d1098fe6e40089df9f73a0c2c1c0b2
jangwoo <zidea2015@163.com>
2019-11-20 19:32:33 +0800

    测试

--- wx/logistic_regression/demo.py
+++ wx/logistic_regression/demo.py
@@ -1,3 +1,23 @@
 # coding=utf-8
 import numpy as np
-import plotly
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from IPython.display import Image 
+# from plotly.offline import donwload
+
+# 定义参数
+theta_1 = 3
+theta_2 = 2
+bias = 0.8
+# 获取网格
+x_1 = y_1 = np.arange(-5, 5, 0.1)
+x_1, y_1 = np.meshgrid(x_1, y_1)
+# 进行二元线性回归模型计算
+y_hat = \
+(theta_1 * np.ravel(x_1) + 
+ theta_2 * np.ravel(y_1) + bias).reshape(x_1.shape)
+
+data = [go.Surface(z=y_hat)]
+layout = go.Layout(title="Linear regression")
+fig = go.Figure(data=data, layout=layout)
+iplot(fig)
