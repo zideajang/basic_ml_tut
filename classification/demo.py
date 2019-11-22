d68fd72c09a8a568f866a02a68a2fdcd312fbdc4
jangwoo <zidea2015@163.com>
2019-11-23 05:41:02 +0800

    提交

---
+++ classification/demo.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/iris.csv",header=None)
+# print(df.info())
+print(df.head())
+
+# plt.figure()
+plt.plot(df[])
+# plt.show()
