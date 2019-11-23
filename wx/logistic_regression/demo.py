0a99fdaa75736c089c16fd51c128674f2dc1c340
zideajang <zidea2015@163.com>
2019-11-23 15:44:09 +0800

    提交线性回归

--- wx/logistic_regression/demo.py
+++ wx/logistic_regression/demo.py
@@ -7,6 +7,21 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 # from plotly.offline import donwload
 
+def logit(p):
+    return np.log(p/(1-p))
+
+
+def draw_logistic_function():
+    p = np.arange(1e-4, 1, 0.0001)
+    y = logit(p)
+    print(y)
+    plt.plot(p, y , color='red', lw=2)
+    plt.axvline(x=0, lw=3, label='undefined')
+    plt.axvline(x=1, lw=2, label='undefined')
+    plt.xticks([i/100 for i in range(0, 125, 25)])
+    plt.grid()
+    plt.legend()
+
 def draw_binary_linear_equation():
     # 定义参数
     theta_1 = 3
@@ -24,8 +39,17 @@ def draw_binary_linear_equation():
     layout = go.Layout(title="Linear regression")
     fig = go.Figure(data=data, layout=layout)
     iplot(fig)
-
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+def draw_sigmoid_equation():
+    z = np.arange(-5, 5, 0.001)
+    y = [sigmoid(i) for i in z]
+    plt.plot(z, y, color='red', lw=2)
+    plt.axhline(y=0, lw=2, label="undefined")
+    plt.axhline(y=1, lw=2, label="undefined")
+    plt.grid()
+    plt.legend()
 if __name__ == "__main__":
-    draw_binary_linear_equation()
+    draw_sigmoid_equation()
     plt.show()
     
